7c1594753152520e015a09e89b3517f962f87436
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2017-10-07 23:54:11 +0900

    Add project

---
+++ gRPCExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gRPCExample
+//
+//  Created by Kyohei Ito on 2017/10/07.
+//  Copyright © 2017年 Kyohei Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
